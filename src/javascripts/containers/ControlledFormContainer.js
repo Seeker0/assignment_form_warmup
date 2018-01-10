157c01482995db34d95f3bc7cb22fe178aca74bf
Jeff Dederick <Xpressivmotion@gmail.com>
2018-01-10 12:50:31 -0500

    initial setup and onSubmit validation complete

---
+++ src/javascripts/containers/ControlledFormContainer.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import ControlledForm from '../components/ControlledForm';
+import { validateForm } from '../helpers';
+
+class ControlledFormContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      success: false,
+      errors: {},
+      exampleEmail: '',
+      examplePassword: '',
+      exampleURL: ''
+    };
+  }
+
+  onChangeInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    let errorObj = validateForm(this.state);
+    this.setState({
+      ...this.state,
+      errors: errorObj
+    });
+    console.log(this.state.errors);
+    if (!this.state.errors) {
+      this.formSuccess();
+    }
+  };
+
+  formSuccess = () => {
+    this.setState(
+      {
+        success: true,
+        errors: {},
+        exampleEmail: '',
+        examplePassword: '',
+        exampleURL: ''
+      },
+      () => console.log('Success!')
+    );
+  };
+
+  render() {
+    return (
+      <ControlledForm
+        onSubmit={this.onSubmit}
+        onChangeInput={this.onChangeInput}
+        {...this.state}
+      />
+    );
+  }
+}
+
+export default ControlledFormContainer;
